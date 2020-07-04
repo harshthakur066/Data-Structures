528793d9384e794b13ba68a11993896b2b2eea17
harshthakur066 <harshthakur066@gmail.com>
2020-07-05 02:49:13 +0530

    power in loops

---
+++ Loops/power.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int power(int m, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    else if (n < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        int i, r = 1;
+        for (i = 1; i < n + 1; i++)
+        {
+            r *= m;
+        }
+        return r;
+    }
+}
+
+int main()
+{
+    cout << power(3, 2);
+    return 0;
+}
