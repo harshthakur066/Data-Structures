afb04d6e7ba17a10c28c871dcb465826629837d5
harshthakur066 <harshthakur066@gmail.com>
2020-11-02 00:01:30 +0530

    doubly linked list props

--- Linked List/doubly_linked_list.js
+++ Linked List/doubly_linked_list.js
@@ -1,30 +1,19 @@
 // 10-->15-->99
 
-let linkedListDemo = {
-  head: {
-    value: 10,
-    next: {
-      value: 15,
-      next: {
-        value: 99,
-        next: null,
-      },
-    },
-  },
-};
-
 class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
       next: null,
+      prev: null,
     };
     this.tail = this.head;
     this.length = 1;
@@ -45,6 +34,7 @@ class LinkedList {
   append(value) {
     const newNode = new Node(value);
 
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
 
@@ -56,6 +46,7 @@ class LinkedList {
     const newNode = new Node(value);
 
     newNode.next = this.head;
+    this.head.prev = newNode;
     this.head = newNode;
 
     this.length++;
@@ -88,7 +79,9 @@ class LinkedList {
     const target = leader.next;
 
     leader.next = newNode;
+    newNode.prev = leader;
     newNode.next = target;
+    target.prev = newNode;
 
     this.length++;
   }
@@ -96,27 +89,28 @@ class LinkedList {
   // Remove or Delete
   remove(index) {
     if (index >= this.length) {
-      //   const leader = this.traverse(this.length - 2);
-      //   leader.next = null;
-      //   this.length--;
-      //   return;
       return this.remove(this.length - 1);
     }
     if (index <= 0) {
       let target = this.head;
       this.head = target.next;
+      this.head.prev = null;
       this.length--;
       return;
     }
     const leader = this.traverse(index - 1);
     const target = leader.next;
-    leader.next = target.next;
+    const joiner = target.next;
+    leader.next = joiner;
+    if (joiner !== null) {
+      joiner.prev = leader;
+    }
 
     this.length--;
   }
 }
 
-const linkedList = new LinkedList(10);
+const linkedList = new DoublyLinkedList(10);
 linkedList.append(15);
 linkedList.append(99);
 linkedList.prepend(1);
@@ -124,7 +118,9 @@ linkedList.prepend(33);
 linkedList.insert(3, 68);
 linkedList.insert(6, 79);
 linkedList.remove(0);
-linkedList.remove(4);
+linkedList.remove(3);
+
+// console.log(linkedList);
 
 const list = linkedList.printList();
 console.log(list.join(" --> "));
