1e4aca5370fd336d3a502184b5569b9c02c194c7
harshthakur066 <harshthakur066@gmail.com>
2021-06-05 02:29:01 +0530

    starting java

---
+++ Hello World/helloworld.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Who are you?", (name) => {
+  console.log(`Hey there ${name}!`);
+  readline.close();
+});
