3d77fbe0ece541050f9b369d4e38faff0cefd744
harshthakur066 <harshthakur066@gmail.com>
2021-06-06 17:15:38 +0530

    Joshephus problem

---
+++ Recursion/Joshephus.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Joshephus {
+    // Complexity - O(n)
+    static int jos(int n, int k) {
+        if (n == 1) {
+            return 0;
+        } else {
+            return ((jos(n - 1, k)) + k) % n;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(jos(5, 3));
+    }
+}
